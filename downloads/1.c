dc7fff33c5663fd6050df51497a29326c592a152
41223218 <147692426+41223218@users.noreply.github.com>
2023-12-28 07:15:44 +0000

    新增練習1

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   :Zhenlin LU\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : September 24 2004\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 09-9999999999\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
